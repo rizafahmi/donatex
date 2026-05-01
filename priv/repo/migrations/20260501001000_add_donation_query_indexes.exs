19488398465202a224e548ede29515efb3a70a06
Riza Fahmi <rizafahmi@gmail.com>
2026-05-01 21:46:39 +0700

    perf(db): index donation recovery query

---
+++ priv/repo/migrations/20260501001000_add_donation_query_indexes.exs
@@ -0,0 +1,9 @@
+defmodule Donatex.Repo.Migrations.AddDonationQueryIndexes do
+  use Ecto.Migration
+
+  def change do
+    create index(:donations, [:status, :alerted, :inserted_at, :id],
+             name: :donations_recovery_queue_idx
+           )
+  end
+end
