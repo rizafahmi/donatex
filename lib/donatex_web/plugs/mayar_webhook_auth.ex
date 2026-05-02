e233d9314d43993e1d89a1fdc2ab6a8903caacde
Riza Fahmi <rizafahmi@gmail.com>
2026-05-02 20:13:49 +0700

    feat(webhooks): log rejected webhook auth requests

--- lib/donatex_web/plugs/mayar_webhook_auth.ex
+++ lib/donatex_web/plugs/mayar_webhook_auth.ex
@@ -3,17 +3,38 @@ defmodule DonatexWeb.Plugs.MayarWebhookAuth do
 
   import Plug.Conn
 
+  require Logger
+
   alias Donatex.Mayar.WebhookAuth
 
   def init(opts), do: opts
 
-  def call(%Plug.Conn{params: %{"token" => token}} = conn, _opts) do
-    if WebhookAuth.valid_token?(token) do
-      conn
-    else
-      conn |> send_resp(404, "Not Found") |> halt()
+  def call(conn, _opts) do
+    case conn.params do
+      %{"token" => token} ->
+        if WebhookAuth.valid_token?(token) do
+          conn
+        else
+          reject(conn, "invalid token")
+        end
+
+      _params ->
+        reject(conn, "missing token")
     end
   end
 
-  def call(conn, _opts), do: conn |> send_resp(404, "Not Found") |> halt()
+  defp reject(conn, reason) do
+    Logger.warning("Mayar webhook rejected #{reason} remote_ip=#{format_remote_ip(conn)}")
+    conn |> send_resp(404, "Not Found") |> halt()
+  end
+
+  defp format_remote_ip(%Plug.Conn{remote_ip: remote_ip}) when is_tuple(remote_ip) do
+    remote_ip
+    |> :inet.ntoa()
+    |> to_string()
+  rescue
+    _ -> "unknown"
+  end
+
+  defp format_remote_ip(_conn), do: "unknown"
 end
