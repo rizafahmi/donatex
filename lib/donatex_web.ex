ba711d856debc027548108ac878f69666b7446ef
Riza Fahmi <rizafahmi@gmail.com>
2026-05-03 08:59:36 +0700

    fix(assets): serve wav file for OBS sound alert

--- lib/donatex_web.ex
+++ lib/donatex_web.ex
@@ -17,7 +17,7 @@ defmodule DonatexWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt smb_stage_clear.wav)
 
   def router do
     quote do
